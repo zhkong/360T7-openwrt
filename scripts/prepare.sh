3055c3d8df6df29eb99ce278efa7f2e5b1739c4f
klarkzh <klarkzh@hotmail.com>
2025-09-05 14:28:21 +0800

    使用openclash，取消mosdns

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -26,6 +26,9 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+## OpenClash
+git clone https://github.com/vernesong/OpenClash.git -b master --single-branch --depth 1 package/openclash
+
 # AutoCore
 mv temp/immortalwrt/package/emortal/autocore package/new/autocore
 # sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/new/autocore/files/luci-mod-status-autocore.json
@@ -42,7 +45,8 @@ mv temp/immortalwrt/package/emortal/default-settings package/new/default-setting
 
 
 # config file
-cp ../config/360t7.config .config
+# cp ../config/360t7.config .config
+cp ../config/openclash.config .config
 make defconfig
 
 rm -rf temp
